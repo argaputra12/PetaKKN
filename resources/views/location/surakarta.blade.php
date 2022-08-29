50c94962331b2b22f4ea867469620e4220656c35
argaputra12 <argaputra@student.uns.ac.id>
2022-08-29 14:55:56 +0700

    update location surakarta

--- resources/views/location/surakarta.blade.php
+++ resources/views/location/surakarta.blade.php
@@ -4,7 +4,10 @@
             Pemetaan Kelompok KKN Kota Surakarta
         </h2>
 
-        <div id='map'></div>
+        <div class="container flex  mx-auto mb-6">
+            <div id="table_data" class="w-2/5 h-[750px] bg-white z-[9999] hidden " style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;"></div>
+            <div id="map" style="width:100%" class="w-full" ></div>
+        </div>
 
     <script type="text/javascript" src="{{ asset('peta/Surakarta.js') }}"></script>
 
@@ -125,54 +128,54 @@ info.update = function(props) {
 
             // marker
             var desa = [
-                ['PASAR KLIWON', -7.581113, 110.830505],
-                ['KAUMAN', -7.572757, 110.825884],
-                ['SEWU', -7.572871, 110.843645],
-                ['PUCANGSAWIT', -7.567869, 110.853693],
-                ['MOJOSONGO', -7.544419, 110.844643],
-                ['JAGALAN', -7.566730, 110.843249],
-                ['KEPATIHAN WETAN',	-7.565246, 110.832167],
-                ['GILINGAN', -7.554372, 110.826787],
-                ['PUNGGAWAN', -7.560864, 110.817334],
-                ['KESTALAN', -7.558898, 110.823015],
-                ['MOJO',	-7.589308, 110.835705],
-                ['PAJANG',	-7.567942, 110.786661],
-                ['TIPES',	-7.577963, 110.811556],
-                ['SEMANGGI',	-7.586578, 110.835410],
-                ['BANYUANYAR',	-7.539024, 110.805365],
-                ['NUSUKAN',	-7.549406, 110.824706],
-                ['DANUSUMAN',	-7.586928, 110.821658],
-                ['KADIPIRO',	-7.534867, 110.821935],
-                ['GANDEKAN',	-7.571709, 110.838501],
-                ['JOGLO',	-7.545279, 110.827029],
-                ['KRATONAN',	-7.578730, 110.817927],
-                ['KARANGASEM',	-7.550126, 110.777397],
-                ['KAMPUNGBARU',	-7.568756, 110.828299],
-                ['BANJARSARI',	-7.545078, 110.817790],
-                ['SUMBER',	-7.547456, 110.803626],
-                ['PURWODININGRAT',	-7.564627, 110.837242],
-                ['LAWEYAN',	-7.570186, 110.794542],
-                ['SERENGAN',	-7.584310, 110.816484],
-                ['SONDAKAN',	-7.565379, 110.792977],
-                ['TEGALHARJO',	-7.559268, 110.834851],
-                ['PASAR KLIWON 2',	-7.590319, 110.830539],
-                ['KAUMAN 2',	-7.574918, 110.824590],
-                ['SEWU 2',	-7.574452, 110.841301],
-                ['PUCANGSAWIT 2',	-7.570657, 110.848064],
-                ['MOJOSONGO 2',	-7.552493, 110.838206],
-                ['JAGALAN 2',	-7.569623, 110.845524],
-                ['KEPATIHAN WETAN  2',	-7.563098, 110.834055],
-                ['GILINGAN 2',	-7.553439, 110.816908],
-                ['PUNGGAWAN 2',	-7.563114, 110.815217],
-                ['KESTALAN 2',	-7.557364, 110.821194],
-                ['PANULARAN',	-7.573227, 110.806404],
-                ['JOYOTAKAN',	-7.592674, 110.820733],
-                ['KEDUNGLUMBU',	-7.574112, 110.831719],
-                ['SETABELAN',	-7.561605, 110.826441],
-                ['MANAHAN',	-7.554849, 110.808091],
-                ['SANGKRAH',	-7.576824, 110.839009],
-                ['SRIWEDARI',	-7.568745, 110.813593],
-                ['PURWOSARI', -7.564716, 110.802262]
+                ['PASAR KLIWON', -7.581113, 110.830505,31],
+                ['KAUMAN', -7.572757, 110.825884,32],
+                ['SEWU', -7.572871, 110.843645,33],
+                ['PUCANGSAWIT', -7.567869, 110.853693,34],
+                ['MOJOSONGO', -7.544419, 110.844643,35],
+                ['JAGALAN', -7.566730, 110.843249,36],
+                ['KEPATIHAN WETAN',	-7.565246, 110.832167,37],
+                ['GILINGAN', -7.554372, 110.826787,38],
+                ['PUNGGAWAN', -7.560864, 110.817334,39],
+                ['KESTALAN', -7.558898, 110.823015,40],
+                ['MOJO',	-7.589308, 110.835705,41],
+                ['PAJANG',	-7.567942, 110.786661,42],
+                ['TIPES',	-7.577963, 110.811556,43],
+                ['SEMANGGI',	-7.586578, 110.835410,44],
+                ['BANYUANYAR',	-7.539024, 110.805365,45],
+                ['NUSUKAN',	-7.549406, 110.824706,46],
+                ['DANUSUMAN',	-7.586928, 110.821658,47],
+                ['KADIPIRO',	-7.534867, 110.821935,48],
+                ['GANDEKAN',	-7.571709, 110.838501,49],
+                ['JOGLO',	-7.545279, 110.827029,50],
+                ['KRATONAN',	-7.578730, 110.817927,51],
+                ['KARANGASEM',	-7.550126, 110.777397,52],
+                ['KAMPUNGBARU',	-7.568756, 110.828299,53],
+                ['BANJARSARI',	-7.545078, 110.817790,54],
+                ['SUMBER',	-7.547456, 110.803626,55],
+                ['PURWODININGRAT',	-7.564627, 110.837242,56],
+                ['LAWEYAN',	-7.570186, 110.794542,57],
+                ['SERENGAN',	-7.584310, 110.816484,58],
+                ['SONDAKAN',	-7.565379, 110.792977,59],
+                ['TEGALHARJO',	-7.559268, 110.834851,60],
+                ['PASAR KLIWON 2',	-7.590319, 110.830539,61],
+                ['KAUMAN 2',	-7.574918, 110.824590,62],
+                ['SEWU 2',	-7.574452, 110.841301,63],
+                ['PUCANGSAWIT 2',	-7.570657, 110.848064,64],
+                ['MOJOSONGO 2',	-7.552493, 110.838206,65],
+                ['JAGALAN 2',	-7.569623, 110.845524,66],
+                ['KEPATIHAN WETAN  2',	-7.563098, 110.834055,67],
+                ['GILINGAN 2',	-7.553439, 110.816908,68],
+                ['PUNGGAWAN 2',	-7.563114, 110.815217,69],
+                ['KESTALAN 2',	-7.557364, 110.821194,70],
+                ['PANULARAN',	-7.573227, 110.806404,71],
+                ['JOYOTAKAN',	-7.592674, 110.820733,72],
+                ['KEDUNGLUMBU',	-7.574112, 110.831719,73],
+                ['SETABELAN',	-7.561605, 110.826441,74],
+                ['MANAHAN',	-7.554849, 110.808091,75],
+                ['SANGKRAH',	-7.576824, 110.839009,76],
+                ['SRIWEDARI',	-7.568745, 110.813593,77],
+                ['PURWOSARI', -7.564716, 110.802262,250]
 
             ];
 
@@ -188,9 +191,42 @@ info.update = function(props) {
 
             for (let i = 0; i < desa.length; i++) {
                 marker = L.marker([desa[i][1], desa[i][2]], {icon: redIcon})
-                .bindPopup(`<b>${desa[i][0]}</b>`)
+                .bindPopup(`<div class="flex flex-col">
+                        <div class="flex">
+                            <div class="w-20">Desa</div>
+                            <div class="">: ${desa[i][0]}</div>
+                        </div>
+                        <div class="flex">
+                            <div class="w-20">Kelompok</div>
+                            <div class="">: 123</div>
+                        </div>
+                        <button class="bg-blue-500 rounded-md font-semibold text-white w-20 h-6 mt-4" onclick="showData(this)">
+                            <input type="hidden" name="desa_id" value="${desa[i][3]}">
+                            Detail
+                        </button>
+                    </div>`)
                 .addTo(map);
                 console.log(marker);
+            }
+
+            const showData = (e) =>{
+
+                const table_data = document.getElementById('table_data');
+                table_data.classList.remove('hidden');
+
+                const desa_id = e.children[0].value;
+
+                $.ajax({
+                    url: `{{ route('location.show') }}`,
+                    type: 'POST',
+                    data: {
+                        'desa_id': desa_id,
+                    },
+                    success: function(data) {
+                        table_data.innerHTML = data;
+                    }
+
+                });
             }
             </script>
     </x-slot>
