fc6127e76965bc55597dab6fe4c8eb024272ec66
argaputra12 <argaputra@student.uns.ac.id>
2022-08-30 22:49:39 +0700

    update boyolali karanganyar modal

--- resources/views/location/boyolali.blade.php
+++ resources/views/location/boyolali.blade.php
@@ -4,9 +4,12 @@
             Pemetaan Kelompok KKN Kabupaten Boyolali
         </h2>
 
-        <div id='map'></div>
-
-        <div id="map"></div>
+        <div class="container flex  mx-auto mb-6">
+            <div id="map" style="width:100%" class="w-full z-10" ></div>
+        </div>
+        <div class="modal-container absolute bg-black bg-opacity-50 inset-0 z-[999] hidden h-screen mt-16 flex justify-center items-center">
+            <div id="table_data" ></div>
+        </div>
 
         <script type="text/javascript" src="{{ asset('peta/Boyolali.js') }}"></script>
 
@@ -142,55 +145,55 @@
 
           // marker
           var desa = [
-                ['GIRIROTO', -7.491981, 110.797119],
-                ['GIRIROTO 2', -7.497699, 110.805940],
-                ['KISMOYOSO', -7.506018, 110.789028],
-                ['KISMOYOSO 2',-7.504636, 110.805486],
-                ['PANDEYAN',-7.517458, 110.790869],
-                ['PANDEYAN 2',-7.516278, 110.804450],
-                ['DONOHUDAN',-7.514164, 110.781167],
-                ['DONOHUDAN 2',-7.523816, 110.788789],
-                ['SAWAHAN',-7.526224, 110.808043],
-                ['GAGAKSIPAT',-7.520445, 110.767189],
-                ['DIBAL',-7.506562, 110.774377],
-                ['MANGGUNG',-7.492671, 110.780926],
-                ['SINDON',-7.503940, 110.759534],
-                ['NGESREP',-7.517476, 110.747417],
-                ['NGARGOREJO',-7.514445, 110.725101],
-                ['SOBOKERTO',-7.501683, 110.735858],
-                ['JEMBUNGAN',-7.550744, 110.686235],
-                ['JEMBUNGAN 2',-7.552479, 110.701024],
-                ['KUWIRAN',-7.543106, 110.703794],
-                ['SAMBON',-7.556110, 110.717142],
-                ['DENGGUNGAN',-7.525653, 110.714680],
-                ['NGARU-ARU',-7.541063, 110.680648],
-                ['BENDAN',-7.545023, 110.672435],
-                ['DUKUH',-7.557019, 110.679556],
-                ['JIPANGAN',-7.558828, 110.691437],
-                ['TANJUNGSARI',-7.515964, 110.683294],
-                ['TRAYU',-7.518285, 110.692630],
-                ['BATAN',-7.533221, 110.703546],
-                ['BANGAK',-7.522801, 110.701927],
-                ['KETAON',-7.530606, 110.676011],
-                ['BANYUDONO',-7.530678, 110.688083],
-                ['BANYUDONO 2',-7.533842, 110.695407],
-                ['CANGKRINGAN',-7.542883, 110.690511],
-                ['JIPANGAN 2',-7.562276, 110.686604],
-                ['GAGAKSIPAT 2',-7.522865, 110.756915],
-                ['DIBAL 2',-7.507257, 110.765482],
-                ['MANGGUNG 2',-7.493883, 110.771278],
-                ['SINDON 2',-7.515716, 110.758507],
-                ['NGARGOREJO 2',-7.523525, 110.726878],
-                ['SOBOKERTO 2',-7.515201, 110.738205],
-                ['KUWIRAN 2',-7.537053, 110.692795],
-                ['SAMBON 2',-7.562602, 110.717591],
-                ['DENGGUNGAN 2',-7.522830, 110.706463],
-                ['DUKUH',-7.556924, 110.679405],
-                ['KARANGANYAR',-7.598256, 110.542133],
-                ['PAGERJURANG',-7.578053, 110.564847],
-                ['MUDAL',-7.502071, 110.616475],
-                ['KRAGILAN',-7.528187, 110.626249],
-                ['BUTUH',-7.553614, 110.632528]
+                ['GIRIROTO', -7.491981, 110.797119, 108],
+                ['GIRIROTO 2', -7.497699, 110.805940, 109],
+                ['KISMOYOSO', -7.506018, 110.789028,110],
+                ['KISMOYOSO 2',-7.504636, 110.805486,111],
+                ['PANDEYAN',-7.517458, 110.790869, 112],
+                ['PANDEYAN 2',-7.516278, 110.804450,113],
+                ['DONOHUDAN',-7.514164, 110.781167,114],
+                ['DONOHUDAN 2',-7.523816, 110.788789,115],
+                ['SAWAHAN',-7.526224, 110.808043,116],
+                ['GAGAKSIPAT',-7.520445, 110.767189,117],
+                ['DIBAL',-7.506562, 110.774377,118],
+                ['MANGGUNG',-7.492671, 110.780926,119],
+                ['SINDON',-7.503940, 110.759534,120],
+                ['NGESREP',-7.517476, 110.747417,121],
+                ['NGARGOREJO',-7.514445, 110.725101,122],
+                ['SOBOKERTO',-7.501683, 110.735858,123],
+                ['JEMBUNGAN',-7.550744, 110.686235,124],
+                ['JEMBUNGAN 2',-7.552479, 110.701024,125],
+                ['KUWIRAN',-7.543106, 110.703794,126],
+                ['SAMBON',-7.556110, 110.717142,127],
+                ['DENGGUNGAN',-7.525653, 110.714680,128],
+                ['NGARU-ARU',-7.541063, 110.680648,129],
+                ['BENDAN',-7.545023, 110.672435,130],
+                ['DUKUH',-7.557019, 110.679556,131],
+                ['JIPANGAN',-7.558828, 110.691437,132],
+                ['TANJUNGSARI',-7.515964, 110.683294,133],
+                ['TRAYU',-7.518285, 110.692630,134],
+                ['BATAN',-7.533221, 110.703546,135],
+                ['BANGAK',-7.522801, 110.701927,136],
+                ['KETAON',-7.530606, 110.676011,137],
+                ['BANYUDONO',-7.530678, 110.688083,138],
+                ['BANYUDONO 2',-7.533842, 110.695407,139],
+                ['CANGKRINGAN',-7.542883, 110.690511,140],
+                ['JIPANGAN 2',-7.562276, 110.686604,141],
+                ['GAGAKSIPAT 2',-7.522865, 110.756915,142],
+                ['DIBAL 2',-7.507257, 110.765482,143],
+                ['MANGGUNG 2',-7.493883, 110.771278,144],
+                ['SINDON 2',-7.515716, 110.758507,145],
+                ['NGARGOREJO 2',-7.523525, 110.726878,146],
+                ['SOBOKERTO 2',-7.515201, 110.738205,147],
+                ['KUWIRAN 2',-7.537053, 110.692795,148],
+                ['SAMBON 2',-7.562602, 110.717591,149],
+                ['DENGGUNGAN 2',-7.522830, 110.706463,150],
+                ['DUKUH',-7.556924, 110.679405,151],
+                ['KARANGANYAR',-7.598256, 110.542133,152],
+                ['PAGERJURANG',-7.578053, 110.564847,153],
+                ['MUDAL',-7.502071, 110.616475,154],
+                ['KRAGILAN',-7.528187, 110.626249,155],
+                ['BUTUH',-7.553614, 110.632528,156]
 
             ];
 
@@ -206,9 +209,42 @@
 
             for (let i = 0; i < desa.length; i++) {
                 marker = L.marker([desa[i][1], desa[i][2]], {icon: redIcon})
-                .bindPopup(`<b>${desa[i][0]}</b>`)
+                .bindPopup(`<div class="flex flex-col py-2">
+                        <div class="flex justify-center">
+                            <div class="font-semibold">KKN DESA ${desa[i][0]}</div>
+                        </div>
+                        <button class="bg-blue-500 rounded-md font-semibold text-white w-20 h-6 mt-4 mx-auto" onclick="showData(this)">
+                            <input type="hidden" name="desa_id" value="${desa[i][3]}">
+                            Detail
+                        </button>
+                    </div>`)
                 .addTo(map);
                 console.log(marker);
+            }
+
+            const showData = (e) =>{
+
+                const table_data = document.querySelector('.modal-container');
+                table_data.classList.remove('hidden');
+
+                const desa_id = e.children[0].value;
+
+                $.ajax({
+                    url: `{{ route('location.show') }}`,
+                    type: 'POST',
+                    data: {
+                        'desa_id': desa_id,
+                    },
+                    success: function(data) {
+                        table_data.innerHTML = data;
+                    }
+
+                });
+            }
+
+            const closeContainer = () =>{
+            const table_data = document.querySelector('.modal-container');
+            table_data.classList.add('hidden');
             }
         </script>
     </x-slot>
--- resources/views/location/karanganyar.blade.php
+++ resources/views/location/karanganyar.blade.php
@@ -4,7 +4,12 @@
             Pemetaan Kelompok KKN Kabupaten Karanganyar
         </h2>
 
-        <div id='map'></div>
+        <div class="container flex  mx-auto mb-6">
+            <div id="map" style="width:100%" class="w-full z-10" ></div>
+        </div>
+        <div class="modal-container absolute bg-black bg-opacity-50 inset-0 z-[999] hidden h-screen mt-16 flex justify-center items-center">
+            <div id="table_data" ></div>
+        </div>
 
     <script type="text/javascript" src="{{ asset('peta/Karanganyar.js') }}"></script>
 
@@ -140,49 +145,82 @@
 
       // marker
       var desa = [
-                ['BLORONG',	-7.643493, 110.988292],
-                ['GEMANTAR',	-7.675537, 111.033041],
-                ['GENENGAN',	-7.665195, 111.008902],
-                ['KEBAK',	-7.676536, 110.991277],
-                ['NGUNUT',	-7.654201, 110.983058],
-                ['SAMBIREJO',	-7.632762, 110.965684],
-                ['SEDAYU',	-7.675775, 110.963375],
-                ['SRINGIN',	-7.650196, 111.016718],
-                ['SUKOSARI',	-7.638639, 110.951335],
-                ['TUGU',	-7.656158, 110.961961],
-                ['TUNGGULREJO',	-7.663385, 111.042384],
-                ['BANDARDAWUNG',	-7.669367, 111.084698],
-                ['BLUMBANG',	-7.661130, 111.155977],
-                ['GONDOSULI',	-7.665483, 111.177692],
-                ['KALISORO',	-7.670076, 111.143197],
-                ['TAWANGMANGU',	-7.673044, 111.127624],
-                ['PLUMBON',	-7.649902, 111.101457],
-                ['KARANGLO',	-7.660939, 111.090002],
-                ['SEPANJANG',	-7.680618, 111.108255],
-                ['TENGKLIK ',	-7.651268, 111.128716],
-                ['PANCOT',	-7.658133, 111.140465],
-                ['WATUSAMBANG',	-7.654187, 111.102004],
-                ['GAYAMDOMPO',	-7.617026, 111.006283],
-                ['JANTIHARJO',	-7.619439, 110.972208],
-                ['POPONGAN',	-7.608726, 110.986813],
-                ['BOLONG',	-7.625952, 110.971864],
-                ['LALUNG ',	-7.612064, 110.938514],
-                ['DAYU ',	-7.476792, 110.843600],
-                ['KARANGLO 2',	-7.659294, 111.078941],
-                ['TAWANGMANGU 2',	-7.668584, 111.116909],
-                ['POJOK',	-7.567470, 111.009132],
-                ['DELINGAN',	-7.598213, 111.002942],
-                ['GEDONG',	-7.578834, 110.981651],
-                ['SEWUREJO',	-7.587222, 111.018373],
-                ['KEMUNING',	-7.594811, 111.116039]
+                ['BLORONG',	-7.643493, 110.988292, 157],
+                ['GEMANTAR',	-7.675537, 111.033041,158],
+                ['GENENGAN',	-7.665195, 111.008902,159],
+                ['KEBAK',	-7.676536, 110.991277,160],
+                ['NGUNUT',	-7.654201, 110.983058,161],
+                ['SAMBIREJO',	-7.632762, 110.965684,162],
+                ['SEDAYU',	-7.675775, 110.963375,163],
+                ['SRINGIN',	-7.650196, 111.016718,164],
+                ['SUKOSARI',	-7.638639, 110.951335,165],
+                ['TUGU',	-7.656158, 110.961961,166],
+                ['TUNGGULREJO',	-7.663385, 111.042384,167],
+                ['BANDARDAWUNG',	-7.669367, 111.084698,168],
+                ['BLUMBANG',	-7.661130, 111.155977,169],
+                ['GONDOSULI',	-7.665483, 111.177692,170],
+                ['KALISORO',	-7.670076, 111.143197,171],
+                ['TAWANGMANGU',	-7.673044, 111.127624,172],
+                ['PLUMBON',	-7.649902, 111.101457,173],
+                ['KARANGLO',	-7.660939, 111.090002,174],
+                ['SEPANJANG',	-7.680618, 111.108255,175],
+                ['TENGKLIK ',	-7.651268, 111.128716,176],
+                ['PANCOT',	-7.658133, 111.140465,177],
+                ['WATUSAMBANG',	-7.654187, 111.102004,178],
+                ['GAYAMDOMPO',	-7.617026, 111.006283,179],
+                ['JANTIHARJO',	-7.619439, 110.972208,180],
+                ['POPONGAN',	-7.608726, 110.986813,181],
+                ['BOLONG',	-7.625952, 110.971864,182],
+                ['LALUNG ',	-7.612064, 110.938514,183],
+                ['DAYU ',	-7.476792, 110.843600,184],
+                ['KARANGLO 2',	-7.659294, 111.078941,185],
+                ['TAWANGMANGU 2',	-7.668584, 111.116909,186],
+                ['POJOK',	-7.567470, 111.009132,187],
+                ['DELINGAN',	-7.598213, 111.002942,188],
+                ['GEDONG',	-7.578834, 110.981651,189],
+                ['SEWUREJO',	-7.587222, 111.018373,190],
+                ['KEMUNING',	-7.594811, 111.116039,261]
 
             ];
 
             for (let i = 0; i < desa.length; i++) {
                 marker = L.marker([desa[i][1], desa[i][2]])
-                .bindPopup(`<b>${desa[i][0]}</b>`)
+                .bindPopup(`<div class="flex flex-col py-2">
+                        <div class="flex justify-center">
+                            <div class="font-semibold">KKN DESA ${desa[i][0]}</div>
+                        </div>
+                        <button class="bg-blue-500 rounded-md font-semibold text-white w-20 h-6 mt-4 mx-auto" onclick="showData(this)">
+                            <input type="hidden" name="desa_id" value="${desa[i][3]}">
+                            Detail
+                        </button>
+                    </div>`)
                 .addTo(map);
                 console.log(marker);
+            }
+
+            const showData = (e) =>{
+
+            const table_data = document.querySelector('.modal-container');
+                table_data.classList.remove('hidden');
+
+                const desa_id = e.children[0].value;
+
+                $.ajax({
+                    url: `{{ route('location.show') }}`,
+                    type: 'POST',
+                    data: {
+                        'desa_id': desa_id,
+                    },
+                    success: function(data) {
+                        table_data.innerHTML = data;
+                    }
+
+                });
+            }
+
+            const closeContainer = () =>{
+                const table_data = document.querySelector('.modal-container');
+                table_data.classList.add('hidden');
             }
     </script>
     </x-slot>
