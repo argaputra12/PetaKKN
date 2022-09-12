ed6129cf659e99221041af4730a9dc329a687a69
argaputra12 <argaputra@student.uns.ac.id>
2022-09-12 10:49:58 +0700

    change interface of data table

--- resources/views/components/table-data.blade.php
+++ resources/views/components/table-data.blade.php
@@ -37,32 +37,6 @@
     <div class="mx-4 border-b border-primary-textgray mb-6 border-opacity-30"></div>
     <div class="flex flex-col justify-start pl-4 mb-4">
         <div class="text-primary-textdark font-semibold mb-3">Program telah berjalan</div>
-        <div class="text-primary-textdark">{{ $data->kelompok->program_telah_jalan }}</div>
+        <div class="text-primary-textdark">• {!! str_replace(", ", "<br/> • ", $data->kelompok->program_telah_jalan)  !!}</div>
     </div>
 </div>
-{{-- <table class="table-fixed text-left border-collapse ">
-    <tr>
-        <th class="w-1/2 pl-2 py-2 border-b-[2px] border-slate-200">Identitas kelompok</th>
-        <td class="p-2 border-b-[2px] pr-2 border-slate-200">{{ $data->kelompok->identitas_kelompok }}</td>
-    </tr>
-    <tr>
-        <th class="w-1/2 pl-2 py-2 border-b-[2px] border-slate-200">Nama ketua</th>
-        <td class="p-2 border-b-[2px] pr-2 border-slate-200">{{ $data->kelompok->nama_ketua }}</td>
-    </tr>
-    <tr>
-        <th class="w-1/2 pl-2 py-2 border-b-[2px] border-slate-200">Sosial media</th>
-        <td class="p-2 border-b-[2px] pr-2 border-slate-200"><a href="{{ $data->kelompok->link_sosmed }}" class="hover:text-primary-blue">Klik disini</a></td>
-    </tr>
-    <tr>
-        <th class="w-1/2 pl-2 py-2 border-b-[2px] border-slate-200">Dokumen penunjang</th>
-        <td class="p-2 border-b-[2px] pr-2 border-slate-200"><a href="{{ $proker->dokumen_penunjang }}" class="hover:text-primary-blue">Klik disini</a></td>
-    </tr>
-    <tr>
-        <th class="w-1/2 pl-2 py-2 border-b-[2px] border-slate-200">Status Keberlanjutan</th>
-        <td class="p-2 border-b-[2px] pr-2 border-slate-200"><a href="{{ $proker->dokumen_penunjang }}" class="hover:text-primary-blue">Klik disini</a></td>
-    </tr>
-    <tr>
-        <th class="w-1/2 pl-2 py-2 align-baseline">Program telah berjalan</th>
-        <td class="p-2  ">{{ $proker->program_telah_jalan }}</td>
-    </tr>
-</table> --}}
